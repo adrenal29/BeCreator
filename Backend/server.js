1c0e9c8a8bb1f689248e496edeeb60feef1162db
Mohit Sharma <mohit29code@gmail.com>
2023-05-13 23:12:44 +0530

    deploy done3

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:"http://34.131.71.8/",credentials:true}));
 
+// Set the Access-Control-Allow-Origin header to allow cross-origin requests from any domain
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   next();
+// });
+
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
